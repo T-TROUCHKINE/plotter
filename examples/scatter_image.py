8fde4583bee30e8db81222f722ac227a25d7a736
Thomas Trouchkine <thomas.trouchkine@gmail.com>
2019-07-23 14:45:33 +0200

    updating scatter

--- examples/scatter_image.py
+++ examples/scatter_image.py
@@ -7,7 +7,7 @@ to_plot = [
         "title": "Matrix Scatter with matrix scale to image",
         "type": "matrixscatter",
         "data": np.random.randint(0, 10, (20, 20)),
-        "image": "img/bcm2837_square.jpg",
+        "image": "img/usa.jpg",
         "revert_y_axis": True,
         "scale_to_image": True,
         "opacity": 0.5
