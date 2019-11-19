460751b05d4b150c0d6deeb517370dc35d108969
Thomas Trouchkine <thomas.trouchkine@gmail.com>
2019-11-19 15:33:39 +0100

    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plotter",
-    version="1.3.6",
+    version="1.3.7",
     author="Thomas Trouchkine",
     author_email="thomas.trouchkine@gmail.com",
     description="Plotting library with descriptive approach",
